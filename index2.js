6fdd6cce5156e1d4a95821743fdd3a4bbc20cbe6
baoxiaoyu02 <baoxiaoyu02@meituan.com>
2021-07-29 14:21:31 +0800

    qweqwe

--- index2.js
+++ index2.js
@@ -1,1 +1,14 @@
 alert(1)
+function quicksort(arr){
+    if(arr.length < 2)return arr
+    var index = Math.floor(arr.length / 2)
+    var val = arr.splice(index, 1)[0]
+    var left = [], right = []
+    for(var i = 0; i < arr.length; ++i){
+        if(arr[i] < val)
+            left.push(arr[i])
+        else
+            right.push(arr[i])
+    }
+    return quicksort(left).concat(val, quicksort(right))
+}
