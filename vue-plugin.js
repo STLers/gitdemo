6758adb5522658369de53781966908785d9eaef4
baoxiaoyu02 <baoxiaoyu02@meituan.com>
2021-07-31 22:57:22 +0800

    qwe

--- vue-plugin.js
+++ vue-plugin.js
@@ -1,8 +1,12 @@
 (function(window){
     const MyPlugin =  {}
     MyPlugin.install = function(Vue, option){
-        Vue.fn1 = function(){}
-        Vue.prototype.fn2 = function(){}
+        Vue.fn1 = function(){
+            console.log("fn1")
+        }
+        Vue.prototype.fn2 = function(){
+            console.log("fn2")
+        }
     }
     window.MyPlugin = MyPlugin
 })(window)
